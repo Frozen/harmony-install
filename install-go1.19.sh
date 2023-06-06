0bcd5d5746d8862e461bea5619c13df2545db1ad
frozen <355847+Frozen@users.noreply.github.com>
2023-06-06 02:32:49 -0400

    initial

---
+++ install-go1.19.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install Go 1.19
+wget https://go.dev/dl/go1.19.9.linux-amd64.tar.gz
+tar xvzf go1.19.9.linux-amd64.tar.gz
+PATHGOBIN="${PWD}/go/bin"
+
+echo $PATHGOBIN >> ~/.profile
+. ~/.profile
+
+
+
+mkdir -p ./go/src/github.com/harmony-one
+cd ./go/src/github.com/harmony-one
+git clone https://github.com/harmony-one/mcl.git
+git clone https://github.com/harmony-one/bls.git
+git clone https://github.com/harmony-one/harmony.git
+cd harmony
+go mod tidy
+make
