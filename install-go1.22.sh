691ab2e4891e377fe8983500fb48a7e4456d41d1
frozen <355847+Frozen@users.noreply.github.com>
2025-02-13 22:30:40 -0400

    1.22

---
+++ install-go1.22.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install Go 1.22
+wget https://go.dev/dl/go1.22.12.linux-amd64.tar.gz
+tar xvzf go1.22.12.linux-amd64.tar.gz
+PATHGOBIN="${PWD}/go/bin"
+
+echo "export PATH=\"\$PATH:$PATHGOBIN\"" >> ~/.profile
+. ~/.profile
+
+
+sudo apt update
+sudo apt install libgmp-dev  libssl-dev  make gcc g++ -y
+
+mkdir -p ./go/src/github.com/harmony-one
+cd ./go/src/github.com/harmony-one
+git clone https://github.com/harmony-one/mcl.git
+git clone https://github.com/harmony-one/bls.git
+git clone https://github.com/harmony-one/harmony.git
+cd harmony
+go mod tidy
+make
